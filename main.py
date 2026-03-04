75ab6119209b281385b49369ce5e1b7b5ef21c3a
Emilia Pahomi <emilia.pahomi@student.usv.ro>
2026-03-04 20:41:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from functii import manhattan_scipy, manhattan_manual, citeste
 
 def main():
-    try:
-        
+    
         vector_a, vector_b = citeste('data.txt')
         print(f"Vector A: {vector_a}")
         print(f"Vector B: {vector_b}")
@@ -16,10 +15,6 @@ def main():
         print(f"Distanta Manhattan (Manual): {dist_manual}")
         print(f"Distanta Manhattan (SciPy):  {dist_scipy}")
 
-    except FileNotFoundError:
-        print("Error: data.txt not found.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
